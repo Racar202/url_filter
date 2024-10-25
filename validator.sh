e79c9b15d1d84b760924a6f80b96d9adf6ed2ff7
Racar202 <41528305+Racar202@users.noreply.github.com>
2024-10-25 14:43:42 +0200

    Create validator.sh

---
+++ validator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+
+  printf '\e[31mNo domains were given. Usage: "./validator.sh <domain1> <domain2> ..."\n\e[0m' >&2
+  exit 1
+
+fi
+
+: >  valid_urls.txt
+
+for (( i=0; i="$#"; i++)) do
+
+ grep "$1" urls.txt >> valid_urls.txt 2>/dev/null || printf "\e[33mWarning: No matches for '%s'\n\e[0m" "$1"  >&2
+ shift
+
+done
+
+if [ -s valid_urls.txt ]; then
+
+ printf "\e[92mOk.\e[0m"
+
+fi
